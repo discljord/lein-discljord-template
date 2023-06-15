9c66543f054abc52ece38094ee198735e10f1659
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-06-15 17:31:43 +0200

    Bump patch version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject discljord/lein-template "0.2.0"
+(defproject discljord/lein-template "0.2.1"
   :description "A leiningen template for discljord projects."
   :url "https://github.com/johnnyjayjay/discljord-template"
   :license {:name "MIT"
